e786b97f9eee33703dc783900670a90063e80b77
Guy Raymond BESSENG A IREH <guy.besseng@gmail.com>
2018-05-10 12:39:10 +0200

    Create mypolyfit.m

---
+++ mypolyfit.m
@@ -0,0 +1,11 @@
+function p = mypolyfit(x,y,n)
+% We must have : length(x) == length(y) and n+1 <= length(x)
+m = length(x); 
+A = ones(m,n+1);
+for i = 1:m
+for j = n:-1:1
+A(i,j) = A(i,j+1)*x(i);
+end
+end
+p = leastsquares(A,y);
+end

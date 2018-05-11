24b48bb6e68ca0a595af376ccdd46f9b67466d21
Guy Raymond BESSENG A IREH <guy.besseng@gmail.com>
2018-05-11 10:46:50 +0200

    Update myhess.m

--- myhess.m
+++ myhess.m
@@ -1,11 +1,14 @@
-function A = myhess(A)
+function H = myhess(A)
 % Hessian matrix of A
 H = A(:);
 n = length(A);
 for k = 1:n-2
     u = H(k+1:n,k);
     rho = -1;
-    a = rho*norm(u)
+    if u(1) < 0
+        rho = 1;
+    end
+    a = rho*norm(u);
     u(1) = u(1) - a;
     u = u/norm(u);
     H(k+1:n,k:n) = H(k+1:n,k:n) - 2*u*(transpose(u)*H(k+1:n,k:n));

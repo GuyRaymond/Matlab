3a5953a44100f292f4badc9bdd013b4b943d4c40
Guy Raymond BESSENG A IREH <guy.besseng@gmail.com>
2018-05-11 13:15:30 +0200

    Update testqr.m

--- testqr.m
+++ testqr.m
@@ -1,5 +1,6 @@
-% A = [ 12 (-51) 4 ; 6 167 (-68);  (-4) 24 (-41)]
-A = [0.8147   0.0975   0.1576; 0.9058 0.2785 0.9706; 0.1270 0.5469 0.9572; 0.9134 0.9575 0.4854; 0.6324 0.9649 0.8003]
+clc
+%A = [ 12 (-51) 4 ; 6 167 (-68);  (-4) 24 (-41)]
+A = [0.8147   0.0975   0.1576;0.9058 0.2785 0.9706; 0.1270 0.5469 0.9572; 0.9134 0.9575 0.4854; 0.6324 0.9649 0.8003]
 [Q1,R1] = qr(A) % with matlab implemention
 A1 = Q1*R1
 Q1*transpose(Q1)
@@ -9,3 +10,7 @@ Q2*transpose(Q2)
 [Q3,R3] = givensqr(A) % with Givens rotations
 A3 = Q3*R3
 Q3*transpose(Q3)
+% With get same results <qr> and <householderqr>
+% Matlab seems to implement QR with the method of Householder reflections
+% Q3 is different than Q1 with <givensqr, but R1 == R2 == R3
+% We have I == Q3*transpose(Q3) and A == Q3*R3 and that is what QR decomposition is about.

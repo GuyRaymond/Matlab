2ee8ae95315f004af5dfd575754c513ca340ad7b
Guy Raymond BESSENG A IREH <guy.besseng@gmail.com>
2018-05-11 10:12:33 +0200

    Update leastsquares.m

--- leastsquares.m
+++ leastsquares.m
@@ -2,10 +2,12 @@ function x = leastsquares(A,y)
 % least squares with QR algorithm
 [m,n] = size(A);
 [Q,R] = myqr(A);
-z = transpose(Q)*y';
+v = zeros(length(y),1);
+v(:) = y(:); % to be more flexible with to enter a row or a column with y
+z = transpose(Q)*v;
 x = zeros(n,1);
 x(n) = z(n)/R(n,n);
 for k = n-1:-1:1
-x(k) = (z(k) - dot(R(k,k+1:n),x(k+1:n))/ R(k,k);
+x(k) = (z(k) - dot(R(k,k+1:n),x(k+1:n)))/ R(k,k);
 end
 end

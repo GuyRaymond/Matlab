243ca566426ebea00e6ae3dd5833da14e202d4ea
Guy Raymond BESSENG A IREH <guy.besseng@gmail.com>
2018-05-11 11:10:32 +0200

    Update givensqr.m

--- givensqr.m
+++ givensqr.m
@@ -1,5 +1,4 @@
 function [Q,R] = givensqr(A)
-% Draft -- not finish
 % QR decomposition with the method of Givens rotations
 [m,n] = size(A);
 Q = eye(m,m);
@@ -10,8 +9,8 @@ r = hypot(R(i,j),R(i-1,j));
 c = R(i-1,j)/r;
 s = R(i,j)/r;
 G = [c s;-s c];
-tG = [c -s;s c];
-R(i-1:i,j:n) = tG*R(i-1:i,j:n); 
-Q(i-1:i,:) = Q(i-1:i,:)*G; 
+R(i-1:i,j:n) = G*R(i-1:i,j:n); 
+Q(i-1:i,:) = G*Q(i-1:i,:); 
 end
+Q = transpose(Q);
 end

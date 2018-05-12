0c63c14229974afd0780a5ebfaddab29243f2c2b
Guy Raymond BESSENG A IREH <guy.besseng@gmail.com>
2018-05-12 11:42:39 +0200

    Update mypolyfit.m

--- mypolyfit.m
+++ mypolyfit.m
@@ -3,9 +3,9 @@ function p = mypolyfit(x,y,n)
 m = length(x); 
 A = ones(m,n+1);
 for i = 1:m
-for j = n:-1:1
-A(i,j) = A(i,j+1)*x(i);
-end
+    for j = n:-1:1
+        A(i,j) = A(i,j+1)*x(i);
+    end
 end
 p = leastsquares(A,y);
 end
